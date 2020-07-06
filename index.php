0246d73e31f22f01b9423d9bffbb8bc4e5e6b200
Sumod Sivadas <contactsumod@gmail.com>
2020-07-06 21:52:39 +0530

    changed index.php

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css"/>
+<!DOCTYPE html >
+<html  ng-app = 'listData'>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta http-equiv="x-UA-Compatible" content="ie=edge" />
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+  </head>
+  <body>
 
-    <title>Homepage</title>
-
-</head>
-<body>
-
-<body ng-app="sumod">
-<header><a href="/angular_controllers">Homepage</a></header><br>
-
-<div class="link">
-    <a href="#!name">Data</a>
-    <div class="ng-view"></div><br>
-
-</div>
-
-
-
-<script src="app/route.js"></script>
-<script src="app/controllers/newController.js"></script>
-
+<div ng-controller = "showData">
+       <table class="table table-striped w-25">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+          
+            <tr ng-repeat = "x in names">
+                <td>{{ x.ID }}</td>
+                <td>{{ x.listitem }}</td>
+            
+            </tr>
+        </table>
     
-</body>
+</div>
+    <script src="app/controllers/controller.js"></script>
+  </body>
 </html>
